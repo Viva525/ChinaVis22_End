49c4c4abf5ea1de85683e4100e7a5a28671a9cd0
Stephenzcc <401186943jrf@gmail.com>
2022-04-14 16:25:30 +0800

    更新了ignore，备份了下数据处理脚本

---
+++ split.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+'''
+split link
+'''
+if not os.path.exists("splitEdge"):
+    os.mkdir("splitEdge")
+
+df = pd.read_csv('./data/Link.csv')
+
+for key, group in tqdm(df.groupby("relation")):
+    # group = group.drop(columns='relation')
+    group.reset_index(drop=True, inplace=True)
+    group.rename(columns={"relation": ":TYPE",
+                          "source": ":START_ID",
+                          "target": ":END_ID"},  inplace=True)
+    group.to_csv(
+        './splitEdge/' + key + '.csv', index=False, encoding='utf_8_sig')
+
+'''
+split node
+'''
+# get work Path
+path = os.getcwd()
+# read csv file
+dataFrame = pd.read_csv(path+"\\data\\Node.csv")
+# group
+groups = dataFrame.groupby("type")
+
+Domain_gp = groups.get_group("Domain")
+IP_gp = groups.get_group("IP")
+Cert_gp = groups.get_group("Cert")
+Register_Name_gp = groups.get_group("Whois_Name")
+Register_Email_gp = groups.get_group("Whois_Email")
+Register_Phone_gp = groups.get_group("Whois_Phone")
+IPC_gp = groups.get_group("IP_C")
+ASN_gp = groups.get_group("ASN")
+
+# split DF
+Domain = Domain_gp.set_index("id")
+IP = IP_gp.drop("industry", axis=1).set_index("id")
+Cert = Cert_gp.drop("industry", axis=1).set_index("id")
+Register_Name = Register_Name_gp.drop("industry", axis=1).set_index("id")
+Register_Email = Register_Email_gp.drop("industry", axis=1).set_index("id")
+Register_Phone = Register_Phone_gp.drop("industry", axis=1).set_index("id")
+IPC = IPC_gp.drop("industry", axis=1).set_index("id")
+ASN = ASN_gp.drop("industry", axis=1).set_index("id")
+
+if not os.path.exists("splitNode"):
+    os.mkdir("splitNode")
+
+Domain.to_csv(path+"\\splitNode\\Domain.csv", encoding='utf_8_sig')
+print("Domain finished")
+IP.to_csv(path+"\\splitNode\\IP.csv", encoding='utf_8_sig')
+print("IP finished")
+Cert.to_csv(path+"\\splitNode\\Cert.csv", encoding='utf_8_sig')
+print("Domain finished")
+Register_Name.to_csv(path+"\\splitNode\\Register_Name.csv",
+                     encoding='utf_8_sig')
+print("Register_Name finished")
+Register_Email.to_csv(
+    path+"\\splitNode\\Register_Email.csv", encoding='utf_8_sig')
+print("Register_Email finished")
+Register_Phone.to_csv(
+    path+"\\splitNode\\Register_Phone.csv", encoding='utf_8_sig')
+print("Register_Phone finished")
+IPC.to_csv(path+"\\splitNode\\IPC.csv", encoding='utf_8_sig')
+print("IPC finished")
+ASN.to_csv(path+"\\splitNode\\ASN.csv", encoding='utf_8_sig')
+print("ASN finished")
