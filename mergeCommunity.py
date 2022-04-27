d0f8c58d2b8489c3872d43f31bc86f34ba1cd43f
Stephenzcc <401186943jrf@gmail.com>
2022-04-27 19:57:14 +0800

    脚本更新

--- mergeCommunity.py
+++ mergeCommunity.py
@@ -12,23 +12,23 @@ del community
 
 with open('modify.csv','a+', encoding='utf-8-sig',newline='') as f:#r为标识符，表示只读
     writer = csv.writer(f)
-    cname = pd.read_csv(
-        './processedData/Edge/domain-cname.csv').drop(columns=[":TYPE", "weight:int"])
-    for i, row in tqdm(cname.iterrows()):
-        c_start = node[node["id"] == row[":START_ID"]]["community"].values[0]
-        c_end = node[node["id"] == row[":END_ID"]]["community"].values[0]
-        if(c_start != c_end):
-            writer.writerow([c_end, c_start])
-            node.loc[node["community"] == c_end, ["community"]] = c_start
+    # cname = pd.read_csv(
+    #     './processedData/Edge/domain-cname.csv').drop(columns=[":TYPE", "weight:int"])
+    # for i, row in tqdm(cname.iterrows()):
+    #     c_start = node[node["id"] == row[":START_ID"]]["community"].values[0]
+    #     c_end = node[node["id"] == row[":END_ID"]]["community"].values[0]
+    #     if(c_start != c_end):
+    #         writer.writerow([c_end, c_start])
+    #         node.loc[node["community"] == c_end, ["community"]] = c_start
         
 
-    # subdomain = pd.read_csv(
-    #     './processedData/Edge/domain-subdomain.csv').drop(columns=[":TYPE", "weight:int"])
-    # for i, row in tqdm(subdomain.iterrows()):
-    #     s_start = node[node["id"] == row[":START_ID"]]["community"].values[0]
-    #     s_end = node[node["id"] == row[":END_ID"]]["community"].values[0]
-    #     if(s_start != s_end):
-    #         writer.writerow([c_end, c_start])
-    #         node.loc[node["community"] == s_end, ["community"]] = s_start
+    subdomain = pd.read_csv(
+        './processedData/Edge/domain-subdomain.csv').drop(columns=[":TYPE", "weight:int"])
+    for i, row in tqdm(subdomain.iterrows()):
+        s_start = node[node["id"] == row[":START_ID"]]["community"].values[0]
+        s_end = node[node["id"] == row[":END_ID"]]["community"].values[0]
+        if(s_start != s_end):
+            writer.writerow([s_end, s_start])
+            node.loc[node["community"] == s_end, ["community"]] = s_start
         
     node.to_csv('CommunityNode.csv', index=False, encoding="utf-8-sig")
